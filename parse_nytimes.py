a5a07e830168297c29aed4cdfc7d6a983821da50
Michael Wheeler <wheeler.m@husky.neu.edu>
2018-02-15 02:18:23 -0500

    Can now parse ingredient data from XML to one or more strings

--- parse_nytimes.py
+++ parse_nytimes.py
@@ -1,15 +1,22 @@
 import os
+import pprint
 from scrapy.selector import Selector
 
 XPATH_RECIPE_NAME = '//*[@itemprop="name"]/text()'
 XPATH_RECIPE_AUTHOR = '//*[@itemprop="author"]/text()'
 XPATH_RECIPE_YIELD = '//*[@itemprop="recipeYield"]/text()'
 XPATH_RECIPE_COOKING_TIME = '//*[@itemprop="cookTime"]/@content'
+XPATH_RECIPE_INGREDIENTS = '//*[@itemprop="recipeIngredient"]'
+XPATH_INGREDIENT_QUANTITY = '//*[@class="quantity"]/text()'
+XPATH_INGREDIENT_PRE_DESC = '//*[@class="ingredient-name"]/text()'
+XPATH_INGREDIENT_CORE_ELEMENT = '//*[@class="ingredient-name"]/span/text()'
+XPATH_INGREDIENT_POST_DESC = '//*[@class="ingredient-name"]/span/following-sibling::text()[1]'
+
 
 # Get the directory that contains the raw recipe data
 for text_file in os.listdir("./nytimes"):
     # Print the text file name
-    #print(text_file)
+    print(text_file)
     
     # Then for every file in that directory
     recipe_file = open(os.path.join(os.getcwd(), "nytimes", text_file), "r").read()
@@ -37,12 +44,37 @@ for text_file in os.listdir("./nytimes"):
     
     # PARSE FOR COOKING TIME
     recipe_cooking_time = Selector(text=recipe_file).xpath(XPATH_RECIPE_COOKING_TIME).extract()
+    #quantity, ingredientName/span
     if recipe_cooking_time:
         recipe_cooking_time = recipe_cooking_time[0]
-    print(recipe_cooking_time)
+    #print(recipe_cooking_time)
     
     
-    # Parse for all the recipe ingredients
+    # PARSE FOR INGREDIENTS
+    recipe_ingredients_html = Selector(text=recipe_file).xpath(XPATH_RECIPE_INGREDIENTS).extract()
+    recipe_ingredients = []
+    for item in recipe_ingredients_html:
+        ingredient_text = ""
+        quantity = Selector(text=item).xpath(XPATH_INGREDIENT_QUANTITY).extract()
+        before_text = Selector(text=item).xpath(XPATH_INGREDIENT_PRE_DESC).extract()
+        core_food_item = Selector(text=item).xpath(XPATH_INGREDIENT_CORE_ELEMENT).extract()
+        after_text = Selector(text=item).xpath(XPATH_INGREDIENT_POST_DESC).extract()
+        if quantity:
+            ingredient_text += quantity[0].strip()
+        if before_text:
+            ingredient_text += " " + before_text[0].strip()
+        if core_food_item:
+            ingredient_text += " " + food_item[0].strip()
+        if after_text:
+            ingredient_text += " " + after_text[0].strip()
+        # Remove extra spaces
+        # TODO -- remove extra spaces like " , "
+        ingredient_text = " ".join(ingredient_text.split())
+        recipe_ingredients.append(ingredient_text)
+    pprint.pprint(recipe_ingredients)
+        
+    
+    
     # Parse for the nutrition information
     # Parse for the recipe instructions
 
